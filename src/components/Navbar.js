2aee4a067e4ddae5c7bcadecdc84570092ea2b42
Nabil Fannane <mzabi30@gmail.com>
2019-02-04 11:59:05 -0800

    test

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import img from '../logo.png'
+import img from './logo.png'
 import styled from 'styled-components'
 import {ButtonContainer} from './button'
 
--- src/components/Product.js
+++ src/components/Product.js
@@ -116,10 +116,11 @@ const Wrapper=styled.div`
     .card{
       border:.1rem solid rgba(0,0,0,0.2);
       box-shadow: 2px 2px 5px 0px rgba(0,0,0,0.2);
+      
 
     }
     .card-footer{
-      background:rgba(247,247,247)
+      background:gray
     }
 
     
